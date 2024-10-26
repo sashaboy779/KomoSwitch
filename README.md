6a8dcd45393423a4ff35e27b48909568f7e247c8
sashaboy779 <sashaboychuk779@gmail.com>
2024-10-26 03:11:50 +0300

    Implement prototype of deskband

---
+++ README.md
@@ -0,0 +1,2 @@
+# komoswitch
+TBD
---
+++ src/KomoSwitch/Deskband.cs
@@ -0,0 +1,22 @@
+﻿using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace KomoSwitch
+{
+    [ComVisible(true)]
+    [Guid("6F42B9CA-592E-4838-95CA-85CD46C57982")]
+    [CSDeskBand.CSDeskBandRegistration(Name = "KomoSwitch", ShowDeskBand = false)]
+    public class Deskband : CSDeskBand.CSDeskBandWin
+    {
+        private static Control _control;
+
+        public Deskband()
+        {
+            Options.MinHorizontalSize = new Size(300, 30);
+            _control = new UserControl1(this);
+        }
+
+        protected override Control Control => _control;
+    }
+}
