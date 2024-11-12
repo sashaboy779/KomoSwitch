8dc0b914db8568a7aeac90fce6f08c6be2ea9ae7
sashaboy779 <sashaboychuk779@gmail.com>
2024-11-13 00:05:06 +0200

    Rename settings.json to config.json

--- src/KomoSwitch/Services/PathManager.cs
+++ src/KomoSwitch/Services/PathManager.cs
@@ -6,7 +6,7 @@ namespace KomoSwitch.Services
     public static class PathManager
     {
         public static string WorkspacesJson => Path.Combine(AppDataFolder, $"{KomoSwitch}\\workspaces.json");
-        public static string SettingsJson => Path.Combine(AppDataFolder, $"{KomoSwitch}\\settings.json");
+        public static string SettingsJson => Path.Combine(AppDataFolder, $"{KomoSwitch}\\config.json");
         public static string LogsFolder => Path.Combine(AppDataFolder, $"{KomoSwitch}\\Logs");
         private static string AppDataFolder => Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
         private static string KomoSwitch => "KomoSwitch";
